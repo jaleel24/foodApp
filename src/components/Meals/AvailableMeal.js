448e856459877dc7bcb9fb81492b6151480fa724
jaleel24 <36284471+jaleel24@users.noreply.github.com>
2022-06-16 18:40:06 -0400

    added  loading state so if there is wait in the response it should show loading

--- src/components/Meals/AvailableMeal.js
+++ src/components/Meals/AvailableMeal.js
@@ -1,47 +1,20 @@
 import { useState, useEffect } from "react";
 import Card from "../UI/Card";
 import MealItems from "./MealItems/MealItems";
-// const DUMMY_MEALS = [
-//   {
-//     id: "m1",
-//     name: "Sushi",
-//     description: "Finest fish and veggies",
-//     price: 22.99,
-//   },
-//   {
-//     id: "m2",
-//     name: "Schnitzel",
-//     description: "A german specialty!",
-//     price: 16.5,
-//   },
-//   {
-//     id: "m3",
-//     name: "Barbecue Burger",
-//     description: "American, raw, meaty",
-//     price: 12.99,
-//   },
-//   {
-//     id: "m4",
-//     name: "Green Bowl",
-//     description: "Healthy...and green...",
-//     price: 18.99,
-//   },
-// ];
-
 
 const AvailableMeals = () => {
   //! to Store meals from backend we need useState
   const[mealL, setMeals] = useState([]);
-
+  const[isLoading, setIsLoading]=useState(true);
   //Always use useffect to handle side effects like fetching the data
   useEffect(()=>{
-    
+    // setIsLoading(true);
     const fetchMeals = async ()=>{
       const response = await fetch('https://reacthttp-e48f0-default-rtdb.firebaseio.com/meals.json');
       const data =  await response.json();
 
       const loadedArray =[];
-
+      //! converting the firebase object into an array so we will run through every key and pull out the data and push it into the array
       for(const key in data){
         loadedArray.push({
           id:key,
@@ -50,28 +23,31 @@ const AvailableMeals = () => {
           price: data[key].price
         })
       }
-      console.log(loadedArray)
       setMeals(loadedArray);
     }
     
     fetchMeals();
+    setIsLoading(false);
   },[]);
 
+  if(isLoading){
+    return <section class="flex justify-center"><p class="text-white text-center  w-1/2">Loading......</p></section>
+  }
  
   const mealList = mealL.map((meals) => {
     return (
-      <MealItems
+     <MealItems
         key={meals.id}
         id={meals.id}
         name={meals.name}
         description={meals.description}
         price={meals.price}
-      />
+      /> 
     );
   });
   return (
     <section>
-      <Card>
+     <Card>
         <ul class="px-12 font-bold font-sans">{mealList}</ul>
       </Card>
     </section>
