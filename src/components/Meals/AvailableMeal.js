b5559fae44d40722aa4b3382c3a1f4714637b77a
jaleel24 <36284471+jaleel24@users.noreply.github.com>
2022-06-20 16:08:26 -0400

    added the erro handling code, if unable to fetch data then throws error

--- src/components/Meals/AvailableMeal.js
+++ src/components/Meals/AvailableMeal.js
@@ -6,14 +6,19 @@ const AvailableMeals = () => {
   //! to Store meals from backend we need useState
   const[mealL, setMeals] = useState([]);
   const[isLoading, setIsLoading]=useState(true);
+  const [hasError, setHasError]=useState();
   //Always use useffect to handle side effects like fetching the data
   useEffect(()=>{
     // setIsLoading(true);
     const fetchMeals = async ()=>{
-      const response = await fetch('https://reacthttp-e48f0-default-rtdb.firebaseio.com/meals.json');
-      const data =  await response.json();
-
+      const response = await fetch('https://reacthttp-e48f0-default-rtdb.firebaseio.com/meals');
       const loadedArray =[];
+      if(!response.ok){
+        setIsLoading(false);
+        throw new Error('Something went wrong');
+      }
+      const data =  await response.json();
+       
       //! converting the firebase object into an array so we will run through every key and pull out the data and push it into the array
       for(const key in data){
         loadedArray.push({
@@ -22,16 +27,25 @@ const AvailableMeals = () => {
           name:data[key].name,
           price: data[key].price
         })
+      
       }
       setMeals(loadedArray);
-    }
     
-    fetchMeals();
-    setIsLoading(false);
+    }
+
+      fetchMeals().catch(error =>{
+        setIsLoading(false);
+        setHasError(error.message);
+      });
+   
+    
   },[]);
 
   if(isLoading){
     return <section class="flex justify-center"><p class="text-white text-center  w-1/2">Loading......</p></section>
+  }
+  if(hasError){
+    return <section class="flex justify-center"><p class="text-orange-600 text-center  w-1/2">{hasError}</p></section>
   }
  
   const mealList = mealL.map((meals) => {
